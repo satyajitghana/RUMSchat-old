ee0819f88d86a629b3a4e06e1dbf6928ba9c1660
Satyajit Ghana <satyajitghana7@gmail.com>
2018-04-01 07:11:52 +0530

    Bare Bones Release
    build : pass
    test : not yet done
    need to add more functionality

--- app/src/main/java/com/example/shadowleaf/rums_chat/Chat.java
+++ app/src/main/java/com/example/shadowleaf/rums_chat/Chat.java
@@ -1,6 +1,120 @@
 package com.example.shadowleaf.rums_chat;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Chat extends AppCompatActivity {
+    LinearLayout layout;
+    RelativeLayout layout_2;
+    Button sendButton;
+    EditText messageArea;
+    ScrollView scrollView;
+    DatabaseReference reference1, reference2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+        layout = findViewById(R.id.layout1);
+        layout_2 = findViewById(R.id.layout2);
+        sendButton = findViewById(R.id.sendButton);
+        messageArea = findViewById(R.id.messageArea);
+        scrollView = findViewById(R.id.scrollView);
+
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+        reference1 = FirebaseDatabase.getInstance().getReferenceFromUrl("https://rums-15e5f.firebaseio.com/messages/" + UserDetails.username + "_" + UserDetails.chatWith);
+        reference2 = FirebaseDatabase.getInstance().getReferenceFromUrl("https://rums-15e5f.firebaseio.com/messages/" + UserDetails.chatWith + "_" + UserDetails.username);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String messageText = messageArea.getText().toString();
+
+                if (!messageText.equals("")) {
+                    Map<String, String> map = new HashMap<String, String>();
+                    map.put("message", messageText);
+                    map.put("username", UserDetails.username);
+                    reference1.push().setValue(map);
+                    reference2.push().setValue(map);
+                    messageArea.setText("");
+                }
+            }
+        });
+
+        reference1.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Map map = dataSnapshot.getValue(Map.class);
+                String message = map.get("message").toString();
+                String userName = map.get("user").toString();
+
+                if (userName.equals(UserDetails.username)) {
+                    addMessageBox("You : \n" + message, 1);
+                }
+                else {
+                    addMessageBox(UserDetails.chatWith + " : \n" + message, 2);
+                }
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public void addMessageBox(String message, Integer type) {
+        TextView textView = new TextView(Chat.this);
+        textView.setText(message);
+
+        LinearLayout.LayoutParams lp2 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        lp2.weight = 1.0f;
+
+        if (type == 1) {
+            lp2.gravity = Gravity.LEFT;
+
+        }
+
+        else {
+            lp2.gravity = Gravity.RIGHT;
+        }
+
+        textView.setLayoutParams(lp2);
+        layout.addView(textView);
+        scrollView.fullScroll(View.FOCUS_DOWN);
+    }
 }
--- app/src/main/java/com/example/shadowleaf/rums_chat/Login.java
+++ app/src/main/java/com/example/shadowleaf/rums_chat/Login.java
@@ -8,8 +8,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Login extends AppCompatActivity {
     TextView registerUser;
@@ -52,7 +61,42 @@ public class Login extends AppCompatActivity {
                     pd.setMessage("Loading . . .");
                     pd.show();
 
-                    //StringRequest request = new String
+                    StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String s) {
+                            if(s.equals("null")) {
+                                Toast.makeText(Login.this, "user not found", Toast.LENGTH_LONG).show();
+                            }
+                            else {
+                                try {
+                                    JSONObject obj = new JSONObject(s);
+                                    if(!obj.has(user)) {
+                                        Toast.makeText(Login.this, "user not found", Toast.LENGTH_LONG).show();
+                                    }
+                                    else if (obj.getJSONObject(user).getString("password").equals(pass)) {
+                                        UserDetails.username = user;
+                                        UserDetails.password = pass;
+                                        startActivity(new Intent(Login.this, Users.class));
+                                    }
+                                    else {
+                                        Toast.makeText(Login.this, "incorrect password", Toast.LENGTH_LONG).show();
+                                    }
+                                } catch (JSONException e){
+                                    e.printStackTrace();
+                                }
+                            }
+                            pd.dismiss();
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            System.out.println("" + error);
+                            pd.dismiss();
+                        }
+                    });
+
+                    RequestQueue rQueue = Volley.newRequestQueue(Login.this);
+                    rQueue.add(request);
                 }
             }
         });
