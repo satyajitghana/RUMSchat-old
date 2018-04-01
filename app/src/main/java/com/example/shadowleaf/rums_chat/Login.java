92734828a05677917b3e43cc3b872ae0bcfd1e1c
Satyajit Ghana <satyajitghana7@gmail.com>
2018-04-01 07:38:48 +0530

    fixed login and register

--- app/src/main/java/com/example/shadowleaf/rums_chat/Login.java
+++ app/src/main/java/com/example/shadowleaf/rums_chat/Login.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -47,7 +48,7 @@ public class Login extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 user = username.getText().toString();
-                pass = username.getText().toString();
+                pass = password.getText().toString();
 
                 if(user.equals("")) {
                     username.setError("can't be blank");
